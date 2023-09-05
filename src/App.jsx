6ef8c5080faa7397e05fb422828ec317a7cbb0ab
Aren Dolbakian <aren.dolbakian@gmail.com>
2023-09-05 12:21:15 -0400

    Created landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import { motion } from 'framer-motion';
+import {BsArrowDown} from 'react-icons/bs'
+
+const projects = [
+	{
+		name: 'Clone-BNB',
+		stack: ['Next.js, TailwindCSS, Prisma, Zustand, Next-Auth, MongoDB'],
+		desc: 'An Airbnb clone built to get familiarized with Next.JS 13. Users can sign up, login, and create listings and reservations',
+		hosted: 'https://clone-bnb-beta.vercel.app/',
+		github: 'https://github.com/ArenRitz/clone-bnb',
+	},
+	{
+		name: 'StartGate',
+		stack: ['React, TailwindCSS, DaisyUI, PostgreSQL'],
+		desc: 'A sleek, customizable personal browser dashboard offering handy widgets and diverse themes for an effortless web experience.',
+		github: 'https://github.com/ArenRitz/final-dashboard',
+	},
+];
 
 function App() {
-  const projects = [
-    {
-      title: 'Project 1',
-      description: 'This is a dummy project 1 description.',
-      techStack: ['React', 'Node.js', 'Express', 'MongoDB'],
-      image: 'https://via.placeholder.com/300x200',
-      githubLink: 'https://github.com/username/project1',
-      liveLink: 'https://project1.example.com',
-    },
-    {
-      title: 'Project 2',
-      description: 'This is a dummy project 2 description.',
-      techStack: ['Angular', 'Firebase'],
-      image: 'https://via.placeholder.com/300x200',
-      githubLink: 'https://github.com/username/project2',
-      liveLink: 'https://project2.example.com',
-    },
-    // Add more projects here
-  ];
-  return (
-    <div className="min-h-screen bg-zinc-800 text-white">
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12"
-      >
-        <header>
-          <h1 className="text-4xl font-bold mb-4">Aren Dolbakian</h1>
-          <h2 className='text-2xl font-semibold mb-4'>Full Stack Web Developer</h2>
-        </header>
-        <section>
-          <h3 className="text-xl font-semibold mb-4">Projects</h3>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {/* Add a similar block for each project */}
-            <motion.div
-              whileHover={{ scale: 1.05 }}
-              className="bg-zinc-700/25 backdrop-blur-[5px] border-zinc-700 border-[1px] p-4 rounded-lg shadow-lg shadow-white/2 "
-            >
-              <h3 className="text-xl font-semibold mb-2">Project 1</h3>
-              <p className="mb-4">Description of project 1</p>
-              <a
-                href="#"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 hover:text-blue-700"
-              >
-                View Project
-              </a>
-            </motion.div>
-          </div>
-        </section>
-        <section className="mt-12">
-          <h2 className="text-2xl font-semibold mb-4">Contact Me</h2>
-          <form>
-            <div className="mb-4">
-              <label htmlFor="name" className="block mb-1">Name</label>
-              <input
-                type="text"
-                id="name"
-                className="w-full border border-gray-300 rounded-lg p-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="email" className="block mb-1">Email</label>
-              <input
-                type="email"
-                id="email"
-                className="w-full border border-gray-300 rounded-lg p-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="message" className="block mb-1">Message</label>
-              <textarea
-                id="message"
-                rows="4"
-                className="w-full border border-gray-300 rounded-lg p-2"
-              ></textarea>
-            </div>
-  <button
-  type="submit"
-  className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-700"
->
-  Submit
-</button>
-</form>
-</section>
-</motion.div>
-</div>
-);
+	return (
+		<>
+			<nav className='h-16 w-full absolute'></nav>
+			<div className='h-screen w-full flex relative'>
+				<div className='m-auto w-[50%] border-l-4 border-green-400'>
+					<div className='ml-12 py-4 flex flex-col gap-6'>
+						<p className='text-green-400 text-2xl font-normal w-fit before:content-["👋_"] before:text-[40px]'>
+							 Hello, my name is
+						</p>
+
+						<p className="text-white  text-[90px]  leading-none after:content-['.'] after:text-green-400 ">
+							Aren Dolbakian
+						</p>
+
+						<div className='w-fit text-[35px]  leading-none text-zinc-500 '>
+							Full Stack Web Developer 💻
+						</div>
+        {/*  */}
+						<p className=' text-2xl'>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							enim ad minim veniam, quis nostrud exercitation ullamco laboris
+						</p>
+
+						<button className='w-fit py-4 px-12 mt-12 border-2 text-lg rounded-lg border-green-400 text-green-400 hover:bg-green-400 hover:text-black'>
+							GET IN TOUCH
+						</button>
+					</div>
+				</div>
+
+        <button className='absolute bottom-8 m-auto left-0 right-0  w-fit pb-2 pt-6 border-2 text-lg rounded-full border-green-400 text-green-400 hover:bg-green-400 hover:text-black'>
+							<BsArrowDown className=" animate-bounce text-4xl"/>
+						</button>
+
+			</div>
+      <div className='h-screen w-full bg-gray-800'>
+      
+      
+      </div>
+
+		</>
+	);
 }
 
 export default App;
